812a1d17830a8b1d870aae5684ea0f9cf6fd2bc2
George <george@bransive.com.au>
2020-08-04 10:37:24 +1000

    Update

---
+++ Projects/P1.1/HelloWorld/HelloWorld/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Message
+    {
+        private string _text;
+
+        public Message(String txt)
+        {
+            _text = txt;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(_text);
+        }
+    }
+}
---
+++ Projects/P1.1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message[] message = new Message[5];
+            message[0] = new Message("Welcome back oh great educator!");
+            message[1] = new Message("What a lovely name");
+            message[2] = new Message("Great name");
+            message[3] = new Message("This name is God!");
+            message[4] = new Message("That is a silly name");
+
+            Console.Write("Enter your name: ");
+
+            string name = Console.ReadLine();
+
+            if (name.ToLower() == "george")
+            {
+                message[0].print();
+            }
+            else if (name.ToLower() == "salim")
+            {
+                message[1].print();
+            }
+            else if (name.ToLower() == "juliana")
+            {
+                message[2].print();
+            }
+            else if (name.ToLower() == "fred")
+            {
+                message[3].print();
+            }
+            else
+            {
+                message[4].print();
+            }
+        }
+    }
+}
